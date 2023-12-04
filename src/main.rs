a9215264ad63a2b95287fd9a699292f658d6ac70
Richard <richardkenyon1991@hotmail.co.uk>
2023-12-04 11:29:29 +0000

    initial working version

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn is_valid_character(c: &char) -> bool {
+    match c {
+        '>' => true,
+        '<' => true,
+        '+' => true,
+        '-' => true,
+        '.' => true,
+        ',' => true,
+        '[' => true,
+        ']' => true,
+        _ => false
+    }
+}
+
+fn main() {
+    let file_name = env::args().skip(1).next().expect("Usage: cargo run -- inputfilename\n");
+
+    let mut program_chars: Vec<char> = Vec::new();
+
+    let file = File::open(file_name).expect("Failed to open file");
+    let mut reader = BufReader::new(file);
+    // buffer to read file into
+    let mut buffer: [u8; 100] = [0; 100];
+
+    let mut byte_count = reader.read(&mut buffer[..]).unwrap();
+    while byte_count > 0 {
+        let mut i = 0;
+        while i < byte_count {
+            let b = buffer[i] as u32;
+            let c = char::from_u32(b).unwrap();
+            if is_valid_character(&c) {
+                program_chars.push(c);
+            }
+            i += 1;
+        }
+        byte_count = reader.read(&mut buffer[..]).unwrap();
+    }
+
+    for c in program_chars.iter() {
+        print!("{c}");
+    }
+    print!("\n");
+
+}
