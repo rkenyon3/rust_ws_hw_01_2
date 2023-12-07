298e9188d496b2761f24957137f5b3be98b041d9
Richard <richardkenyon1991@hotmail.co.uk>
2023-12-07 10:03:10 +0000

    remove calls to unwrap and improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::env;
+use std::env::args;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::error::Error;
 
 fn is_valid_character(c: &char) -> bool {
     match c {
@@ -17,8 +18,8 @@ fn is_valid_character(c: &char) -> bool {
     }
 }
 
-fn main() {
-    let file_name = env::args().skip(1).next().expect("Usage: cargo run -- inputfilename\n");
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_name = args().nth(1).ok_or("Usage: cargo run -- inputfilename")?;
 
     let mut program_chars: Vec<char> = Vec::new();
 
@@ -28,18 +29,18 @@ fn main() {
     let mut buffer: [u8; 100] = [0; 100];
 
     // read the file contents in chunks, and store program characters in program_chars
-    let mut byte_count = reader.read(&mut buffer[..]).unwrap();
+    let mut byte_count = reader.read(&mut buffer[..])?;
     while byte_count > 0 {
         let mut i = 0;
         while i < byte_count {
             let b = buffer[i] as u32;
-            let c = char::from_u32(b).unwrap();
+            let c = char::from_u32(b).ok_or("failed to parse character")?;
             if is_valid_character(&c) {
                 program_chars.push(c);
             }
             i += 1;
         }
-        byte_count = reader.read(&mut buffer[..]).unwrap();
+        byte_count = reader.read(&mut buffer[..])?;
     }
 
     // print the bf program to the screen
@@ -47,5 +48,5 @@ fn main() {
         print!("{c}");
     }
     print!("\n");
-
+    Ok(())
 }
